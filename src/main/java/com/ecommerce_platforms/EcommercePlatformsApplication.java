064cee9a41f605038499da016d60bc047871513b
JordanDeev <prograllumitaxi@outlook.com>
2024-11-10 18:15:49 -0500

    FirstCommit

---
+++ src/main/java/com/ecommerce_platforms/EcommercePlatformsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_platforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePlatformsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce_platforms/EcommercePlatformsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_platforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
